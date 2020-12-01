3265d9536dabd21ada2af102237d237a74267ca5
yoshi-automation <yoshi-automation@google.com>
2020-12-01 06:19:47 -0800

    feat!: Updated third_party Cloud Build clients with new Build message fields: - `service_account`, which is available to members of our closed alpha - CLOUD_LOGGING_ONLY logging modes - `dynamic_substitutions` option - resource annotations added
    PiperOrigin-RevId: 331027363
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Sep 10 15:15:28 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bc18612d35891d88bcac57c85d79db93f4f29028
    Source-Link: https://github.com/googleapis/googleapis/commit/bc18612d35891d88bcac57c85d79db93f4f29028

--- google/cloud/dialogflowcx_v3beta1/__init__.py
+++ google/cloud/dialogflowcx_v3beta1/__init__.py
@@ -230,7 +230,6 @@ __all__ = (
     "OutputAudioEncoding",
     "Page",
     "PageInfo",
-    "PagesClient",
     "QueryInput",
     "QueryParameters",
     "QueryResult",
@@ -239,6 +238,7 @@ __all__ = (
     "SessionEntityType",
     "SessionEntityTypesClient",
     "SessionInfo",
+    "SessionsClient",
     "SpeechModelVariant",
     "SpeechToTextSettings",
     "SpeechWordInfo",
@@ -269,5 +269,5 @@ __all__ = (
     "WebhookRequest",
     "WebhookResponse",
     "WebhooksClient",
-    "SessionsClient",
+    "PagesClient",
 )
