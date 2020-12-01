d0dda00c644de28281ef9e2fa814c7b5320b2436
yoshi-automation <yoshi-automation@google.com>
2020-12-01 06:19:47 -0800

    chore: update python microgen version to 0.33.0
    PiperOrigin-RevId: 331031262
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Sep 10 15:35:17 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 6a18a3c50ffd62de53dd2e44f599d6696580f90b
    Source-Link: https://github.com/googleapis/googleapis/commit/6a18a3c50ffd62de53dd2e44f599d6696580f90b

--- google/cloud/dialogflowcx_v3beta1/__init__.py
+++ google/cloud/dialogflowcx_v3beta1/__init__.py
@@ -230,6 +230,7 @@ __all__ = (
     "OutputAudioEncoding",
     "Page",
     "PageInfo",
+    "PagesClient",
     "QueryInput",
     "QueryParameters",
     "QueryResult",
@@ -251,7 +252,6 @@ __all__ = (
     "TrainFlowRequest",
     "TransitionRoute",
     "TransitionRouteGroup",
-    "TransitionRouteGroupsClient",
     "UpdateAgentRequest",
     "UpdateEntityTypeRequest",
     "UpdateEnvironmentRequest",
@@ -269,5 +269,5 @@ __all__ = (
     "WebhookRequest",
     "WebhookResponse",
     "WebhooksClient",
-    "PagesClient",
+    "TransitionRouteGroupsClient",
 )
