56a0cca96486cd6100fb1ce09f600feb96037bda
Anthonios Partheniou <partheniou@google.com>
2021-04-27 12:21:10 -0400

    chore: migrate to owl bot

--- owlbot.py
+++ owlbot.py
@@ -22,19 +22,9 @@ from synthtool.languages import python
 gapic = gcp.GAPICBazel()
 common = gcp.CommonTemplates()
 
-# ----------------------------------------------------------------------------
-# Generate dialogflow CX GAPIC layer
-# ----------------------------------------------------------------------------
-versions = ["v3beta1", "v3"]
+default_version = "v3"
 
-for version in versions:
-    library = gapic.py_library(
-        service="dialogflow",
-        version=version,
-        bazel_target=f"//google/cloud/dialogflow/cx/{version}:dialogflow-cx-{version}-py",
-
-    )
-
+for library in s.get_staging_dirs(default_version):
     s.move(
         library,
         excludes=[
@@ -42,9 +32,11 @@ for version in versions:
             "docs/index.rst",
             "README.rst",
             "noxfile.py",
-            f"scripts/fixup_dialogflowcx_{version}_keywords.py",
+            "scripts/fixup_dialogflowcx_{}_keywords.py".format(library.name),
         ],
     )
+
+s.remove_staging_dirs()
 
 # ----------------------------------------------------------------------------
 # Add templated files
