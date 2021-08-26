a7ef32a3fb6009978d36c7cb0bc6ceac56ef06ea
Gal Zahavi <galzahavi@google.com>
2021-08-26 11:19:50 -0700

    changed pytest scope to session

--- samples/snippets/page_management_test.py
+++ samples/snippets/page_management_test.py
@@ -42,7 +42,7 @@ def loop():
     loop.close()
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def setup_teardown():
     loop = asyncio.new_event_loop()
     agentName = "temp_agent_" + str(uuid.uuid4())
