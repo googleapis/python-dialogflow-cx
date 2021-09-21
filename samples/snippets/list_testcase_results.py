aea11405b7bafa3570dbc19b9765183780b850e6
Gal Zahavi <galzahavi@google.com>
2021-09-21 13:31:10 -0700

    fixed failing test

--- samples/snippets/list_testcase_results.py
+++ samples/snippets/list_testcase_results.py
@@ -21,10 +21,10 @@ from google.cloud.dialogflowcx_v3.types.test_case import ListTestCaseResultsRequ
 def list_test_case(project_id, agent_id, test_id, location):
 
     req = ListTestCaseResultsRequest()
-    req.parent = f"projects/${project_id}/locations/${location}/agents/${agent_id}/testCases/${test_id}"
+    req.parent = f"projects/{project_id}/locations/{location}/agents/{agent_id}/testCases/{test_id}"
     req.filter = "environment=draft"
     client = TestCasesClient(
-        client_options={"api_endpoint": "global-dialogflow.googleapis.com"}
+        client_options={"api_endpoint": f"{location}-dialogflow.googleapis.com"}
     )
     #Makes a call to list all test case results that mathc filter
     result = client.list_test_case_results(request=req)    
