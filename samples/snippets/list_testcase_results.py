9ac890b9d68dd656c0a706319eeda2834c726792
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2021-09-22 09:26:28 -0700

    Update samples/snippets/list_testcase_results.py
    Co-authored-by: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>

--- samples/snippets/list_testcase_results.py
+++ samples/snippets/list_testcase_results.py
@@ -26,7 +26,7 @@ def list_test_case(project_id, agent_id, test_id, location):
     client = TestCasesClient(
         client_options={"api_endpoint": f"{location}-dialogflow.googleapis.com"}
     )
-    # Makes a call to list all test case results that mathc filter
+    # Makes a call to list all test case results that match filter
     result = client.list_test_case_results(request=req)
     print(result)
     return result
