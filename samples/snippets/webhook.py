37f6e1a9711ccee1d44673fd87bea41195b151eb
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2021-10-14 14:34:09 -0700

    lint fix

--- samples/snippets/webhook.py
+++ samples/snippets/webhook.py
@@ -11,9 +11,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 ''' handle_webhook will return the correct fullfilment response dependong the tag that is sent in the request'''
+
 # [START dialogflow_cx_webhook]
+
+
 def handle_webhook(request):
 
     req = request.get_json()
