09eb2e86db1798324fc651017da15a35924a2347
Nick Cain <nicholascain@google.com>
2022-04-28 22:59:11 +0000

    Update module docstring for the validate form parameter snippet.

--- samples/snippets/webhook_validate_form_parameter_test.py
+++ samples/snippets/webhook_validate_form_parameter_test.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test webhook"""
+"""Test validate form parameter webhook snippet."""
 
 import flask
 import pytest
