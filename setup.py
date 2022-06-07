989b056b0a8af2625be95c916e08fe7fdc252fe1
Anthonios Partheniou <partheniou@google.com>
2022-06-07 22:46:56 +0000

    fix(deps): require protobuf<4.0.0 on v0 branch

--- setup.py
+++ setup.py
@@ -46,8 +46,9 @@ setuptools.setup(
     include_package_data=True,
     install_requires=(
         "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
-        "proto-plus >= 1.4.0",
-        "packaging >= 14.3",
+        "proto-plus >= 1.4.0, <2.0.0dev",
+        "packaging >= 14.3, <22.0.0dev",
+        "protobuf<4.0.0dev",
     ),
     python_requires=">=3.6",
     classifiers=[
